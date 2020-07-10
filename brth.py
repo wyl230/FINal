a069360f24d2e02e116757b32c781908c4c4c9ba
wyl230 <1900012779@pku.edu.cn>
2020-07-10 16:20:00 +0800

    bak before reconstruction

--- brth.py
+++ brth.py
@@ -1,10 +1,12 @@
 import pgzrun
 # import globalValues
 import time
+import end_of_battle as eb 
 from math import *
 from random import *
 from somefunc import *
 from pre_written import *
+from star import *
 from confront import *
 from rainstorm import *
 from button import *
@@ -29,6 +31,7 @@ LINE_COLOR = 'gold'
 cur_time = 0.0
 cnt = 0
 rab_live = True
+
 start_time = time.time()
 rain = Draw_rain(100, 150)
 randcolors = [choice(COLORS) for i in range(20)]
@@ -48,6 +51,8 @@ class Gameclass:
         self.game_on = False
         self.preparing = False
         self.click_cnt = 0
+        self.win_battle = False 
+        self.battle_end = False 
         # self.game_on = True
         self.game_message = 'fine'
         self.reset()
@@ -67,104 +72,8 @@ WIDTH = 1000
 HEIGHT = 562  # 1000 * 9 // 16
 MIDDLE = WIDTH//2, HEIGHT//2
 start_pic = Actor('gamestart', (1000//2, 562//2))
-ACCEL = 1.0
-DRAG = 0.9
-TRAIL_LENGTH = 2
-MIN_WRAP_FACTOR = 0.1
-BOUNDS = Rect(0, 0, WIDTH, HEIGHT)
+
 FONT = 'eunomia_regular'
-
-warp_factor = MIN_WRAP_FACTOR
-centerx = WIDTH // 2
-centery = HEIGHT // 2
-stars = []
-
-
-class Star:
-    __slots__ = (
-        'pos', 'vel', 'brightness', 'speed', 'position_history'
-    )
-
-    def __init__(self, pos, vel):
-        self.pos = pos
-        self.vel = vel
-        self.brightness = 10
-        self.speed = hypot(*vel)
-
-    @property
-    def end_pos(self):
-        x, y = self.pos
-        vx, vy = self.vel
-
-        return (
-            x - vx * warp_factor * TRAIL_LENGTH / 60,
-            y - vy * warp_factor * TRAIL_LENGTH / 60,
-        )
-
-
-lastc = (255, 255, 255)
-
-
-def draw_stars():
-    def f(): return randint(10, 255)  # brighter
-    global lastc
-
-    if randint(1, 60) != 1:
-        color = lastc
-    else:
-        color = (f(), f(), f())
-    lastc = color
-    for star in stars:
-        b = star.brightness
-        cur_color = (int(i*b/5) for i in color)
-        # color = (b*2,b*2,b*2)
-        # color = (1,111,11)
-        screen.draw.line(star.end_pos, star.pos, color)
-
-
-def update_stars(dt):
-    global stars, warp_factor
-    warp_factor = (
-        MIN_WRAP_FACTOR + (warp_factor - MIN_WRAP_FACTOR) * DRAG ** dt
-    )
-
-    while len(stars) < 300:
-        # Pick a direction and speed
-        angle = uniform(-pi, pi)
-        speed = 255 * uniform(0.3, 1.0) ** 2
-
-        # Turn the direction into position and velocity vectors
-        dx = cos(angle)
-        dy = sin(angle)
-        d = uniform(25 + TRAIL_LENGTH, 100)
-        pos = centerx + dx * d, centery + dy * d
-        vel = speed * dx, speed * dy
-
-        stars.append(Star(pos, vel))
-
-    # Update the positions of stars
-    for s in stars:
-        x, y = s.pos
-        vx, vy = s.vel
-
-        # Move according to speed and warp factor
-        x += vx * warp_factor * dt
-        y += vy * warp_factor * dt
-        s.pos = x, y
-
-        # Grow brighter
-        s.brightness = min(s.brightness + warp_factor * 200 * dt, s.speed)
-
-        # Get faster
-        s.vel = vx * 2 ** dt, vy * 2 ** dt
-
-    # Drop any stars that are completely off-screen
-    stars = [
-        star
-        for star in stars
-        if BOUNDS.collidepoint(star.end_pos)
-    ]
-
 
 game = Gameclass()
 all_actors = [Actor('poke9', rand_pos()), Actor('poke', rand_pos()), Actor('poke2', rand_pos()), Actor('poke3', rand_pos()), Actor('poke4', rand_pos(
@@ -222,13 +131,18 @@ def check_death():
         # clock.schedule_unique(draw, 1.0)
         # clock.schedule_unique(update, 1.0)
         # print('you lose')
+        game.battle_end = True 
+        game.win_battle = False 
         game.confronting = False
         game.on = False
         the_one.hp = 1000
     elif not opposite:
         print('you win')
         game.confronting = False
-        game.on = False  # 合并之后改为true
+        game.battle_end = True 
+        game.win_battle = True
+        # game.on = False  # 合并之后改为true
+        game.on = True 
         the_one.hp = 1000
         opposite = [Role(Actor('pokemon2s', rand_pos()))
                     for _ in range(randint(3, 5))]
@@ -354,22 +268,30 @@ def draw_start(screen):
     for t in texts:
         t.draw() 
 
+def draw_end_battle():
+    if game.win_battle:
+        eb.draw_win()
+    else:
+        eb.draw_lose() 
 def draw():
     global TITLE
     screen.clear()
     if not game.on:
         draw_start(screen)
-        draw_stars()
+        draw_stars(screen)
         # screen.fill('red')
         # screen.blit('background',(0,0))
         #
         return
     if game.preparing:
         draw_preparation(screen)
-        draw_stars()
+        draw_stars(screen)
         return
+    if game.battle_end:
+        draw_end_battle() 
+        return 
     if game.confronting:
-        TITLE = 'nothing can be done now..'
+        TITLE = '未名湖就是这个样子的'
         draw_confront()
         update_confront()
         return
@@ -404,15 +326,15 @@ def on_mouse_down(pos):
 
 
 def on_mouse_move(pos):
-    global centerx, centery
     if not game.on:
         if start_pic.collidepoint(pos):
             start_pic.angle = randint(-13, 13)
         else:
             start_pic.angle = 0
-            centerx, centery = pos
+            set_star_point(pos) 
         return
     if game.preparing:
+        set_star_point(pos) 
         centerx, centery = pos
         return
 
@@ -432,6 +354,10 @@ def on_key_down(key):
         if key == keys.J and the_one.mp >= 100:
             game.raining = True
         return
+    if game.battle_end:
+        if key == keys.SPACE:
+            game.battle_end = False 
+            game.on = False # temporary
 
 
 def on_key_up(key):
---
+++ cur_state.py
@@ -0,0 +1,38 @@
+
+# 当前玩家所具有的能力 
+
+import pgzrun
+# import globalValues
+import time
+import end_of_battle as eb 
+from math import *
+from random import *
+from somefunc import *
+from pre_written import *
+from confront import *
+from rainstorm import *
+from button import *
+from pgzero.actor import Actor
+from pgzero.loaders import sounds
+from pgzero.keyboard import keys
+from pgzero.rect import Rect, ZRect
+from pgzero.loaders import sounds, images
+from pgzero import music, tone
+from pgzero.clock import clock
+from pgzero.builtins import keymods  # 似乎没有作用
+from pgzero.constants import mouse
+from pgzero.animation import animate
+from pgzero.keyboard import keys, Keyboard
+from pgzero.screen import Screen
+keyboard: Keyboard  # 类型标注
+screen: Screen  # 类型标注
+# skills: point scherm drift rain
+class State:
+    def __init__(self):
+        self.has_skills = [False if _ else True for _ in range(4) ] 
+        self.hp = 1000
+        self.mp = 800 
+        self.shake = False 
+        self.skills_power = [5 for _ in range(4)]
+        # self.skill.consume = 
+        self.purification_capacity = 2 
